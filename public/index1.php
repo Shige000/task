8ac0980338f7c3eac8b8a428dbf7c5dbe26ce3a6
Yoneshige <yoneshige@questar.ac>
2022-06-07 23:00:16 +0900

    PHP記述

---
+++ public/index1.php
@@ -0,0 +1,27 @@
+<?php
+
+$array=[
+'name'=>'本田',
+'height'=>'90',
+'sports'=>'sakka'
+];
+
+echo $array['sports'];
+
+echo '<are>';
+ver_dump($array);
+echo '</are>';
+
+'namea'=>
+[
+$array=['name'=>'本田',
+'height'=>'90',
+'sports'=>'sakka'
+],
+'kakawa'=>[
+
+    'height'=>168,
+    'hobby'=>'サッカー'
+]
+];
+?>
